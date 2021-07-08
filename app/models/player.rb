b224de6f3e2db2f404344a6edd0c77c2f29fc943
koshea9 <keegan.oshea9@gmail.com>
2021-07-08 15:56:10 -0600

    add validation to player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,9 @@ class Player < ApplicationRecord
   has_many :followees, through: :followed_players
   has_many :following_players, foreign_key: :followee_id, class_name: "Friendship"
   has_many :followers, through: :following_players
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Player, type: :model do
     it { should have_many :followers }
     it { should have_many :followees }
   end
+
+  describe "validations" do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :phone }
+    it { should validate_presence_of :email }
+  end
 end
