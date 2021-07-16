86d36a4f7769d09d35d50fc0e5f05a4178422a2c
koshea9 <keegan.oshea9@gmail.com>
2021-07-16 16:31:27 -0600

    add error handling for not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :handle_parameter_missing
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActionController::RoutingError, with: -> { render_404 }
 
   def handle_parameter_missing(exception)
-    render json: { errors: [ { "code": 400, "message": exception.message }]}, status: :bad_request
+    render json: { errors: [ { "code": 400, "message": exception.record.errors }]}, status: :bad_request
+  end
+
+  def render_not_found_response(exception)
+    render json: { errors: [ { "code": 404, "message": exception.message }]}, status: :not_found
   end
 end
