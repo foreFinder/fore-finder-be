620686d527e6418c1dc39319d1cd4f7890a10190
koshea9 <keegan.oshea9@gmail.com>
2021-07-17 14:57:47 -0600

    WIP destroy event

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get "/events", to: "events#index"
       get "/event/:id", to: "events#show"
       post "/event", to: "events#create"
+      delete "/event/:id", to: "events#destroy"
       get "/players", to: "players#index"
       patch "/player-event", to: "player_events#update"
       get "/players/:player_id/events", to: "events#index"
