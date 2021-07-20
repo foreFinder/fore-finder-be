961ace9805c6d851d149fdaae3fbcb8ecda3079d
koshea9 <keegan.oshea9@gmail.com>
2021-07-19 20:40:18 -0600

    WIP

--- app/models/player_event.rb
+++ app/models/player_event.rb
@@ -8,4 +8,16 @@ class PlayerEvent < ApplicationRecord
     declined: 2,
     closed: 3
   }
+
+  def self.close_invitations(invite)
+    event = Event.find(invite[:event_id])
+    invitations = all.where(event_id: event)
+    if event.calculate_remaining_spots == 0
+      invitations.map do |invite|
+        if invite.invite_status == "pending"
+          invite.invite_status = "closed"
+        end
+      end
+    end
+  end
 end
