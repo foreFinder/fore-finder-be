f12e8c0dca215d0a809a128430e9aa6539cb0fae
koshea9 <keegan.oshea9@gmail.com>
2021-07-20 15:08:36 -0600

    add twilio env file in place of yml from figaro

--- send_sms.rb
+++ send_sms.rb
@@ -1,15 +1,12 @@
 # Download the twilio-ruby library from twilio.com/docs/libraries/ruby
 require 'twilio-ruby'
 
-account_sid = 'ACb2cbb6258e9068a2a4a9ae3499d305c7'
-auth_token = '298fc9d50151f6315338e2a720e16649'
-client = Twilio::REST::Client.new(account_sid, auth_token)
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+message = @client.messages.create(
+    body: "Hello from Ruby",
+    to: "+12533597214",    # Replace with your phone number
+    from: "+12406161914")  # Use this Magic Number for creating SMS
 
-from = '+12406161914' 
-to = '+12533597214' 
-
-client.messages.create(
-from: from,
-to: to,
-body: "It works?!?!?!"
-)
+puts message.sid
