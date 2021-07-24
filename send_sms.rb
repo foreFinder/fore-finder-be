c662b7d3ce2d52220b3bf971bf8f15cbbc9c3028
koshea9 <keegan.oshea9@gmail.com>
2021-07-24 15:36:39 -0600

    refactor to replace ruby with ar

--- send_sms.rb
+++ send_sms.rb
@@ -5,8 +5,8 @@ account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 message = @client.messages.create(
-    body: "Hello from Ruby",
-    to: "+12533597214",    # Replace with your phone number
+    body: "You've been invited to tee-time",
+    to: "+13197952720",    # Replace with your phone number
     from: "+12406161914")  # Use this Magic Number for creating SMS
 
 puts message.sid
