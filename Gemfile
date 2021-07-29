bf79aadad89183a3c409d4c686357668170e89b7
koshea9 <keegan.oshea9@gmail.com>
2021-07-29 15:08:43 -0600

    add vcr and webmock

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ group :development, :test do
   gem 'shoulda-matchers'
   gem 'pry'
   gem 'dotenv-rails'
+  gem 'vcr'
+  gem 'webmock'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  VCR.configure do |config|
+    config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+    config.hook_into :webmock
+    config.default_cassette_options = { re_record_interval: 100.days }
+    config.configure_rspec_metadata!
+  end
+
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
        with.test_framework :rspec
