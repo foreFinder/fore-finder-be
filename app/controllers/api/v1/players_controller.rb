6e8970248dfb1a5053899f679c6a634d4134966c
koshea9 <keegan.oshea9@gmail.com>
2021-08-20 20:30:17 -0600

    add specs for error handling user registration

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -7,8 +7,10 @@ class Api::V1::PlayersController < ApplicationController
   def create
     player = player_params
     player[:email] = player[:email].downcase
-    new_player = Player.create(player)
-    render json:PlayersSerializer.new(new_player), status: 201
+    new_player = Player.create!(player)
+    if new_player.save
+      render json:PlayersSerializer.new(new_player), status: 201
+    end
   end
 
   private
