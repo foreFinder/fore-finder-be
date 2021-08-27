7d2344b9649000f57b9f21e667e2dfb48711d48c
koshea9 <keegan.oshea9@gmail.com>
2021-08-27 09:36:37 -0600

    add error handling for email not found in login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = Player.find_by_email(params[:email])
+    user = Player.find_by_email!(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       render json: PlayersSerializer.new(user), status: 200
